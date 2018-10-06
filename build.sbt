54e4e73b1d99f736430847e50ca2775cc5382990
naseem91 <naseemmahasneh1991@gmail.com>
2018-10-06 12:15:24 +0300

    dot product

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "DotProduct"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
---
+++ src/main/scala/InnerProduct.scala
@@ -0,0 +1,14 @@
+object InnerProduct {
+  def main(args: Array[String]): Unit = {
+    val list1:List[Double]=List(1,2,3)
+    val list2:List[Double] =List(1,2,3)
+    println(list1 + " . " + list2)
+    println(dotproduct(list1,list2))
+  }
+  def dotproduct(listA:List[Double],listB:List[Double]):Double ={
+    var tms:List[Double]=times(listA,listB)
+    pls(tms)
+  }
+  def times(list1:List[Double],list2:List[Double]):List[Double]=list1.zip(list2).map { case (a, b) => a * b }
+  def pls(list:List[Double])=list.fold(0.0)(_+_)
+}
