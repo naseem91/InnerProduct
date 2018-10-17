d9297ecc1916cfa9c31eba253914780c5d7f2183
naseem91 <naseemmahasneh1991@gmail.com>
2018-10-17 10:09:12 +0300

    Solving task 3 (inner product using for, while,foreach ,foldleft,map,recursion)

--- src/main/scala/InnerProductExamples.scala
+++ src/main/scala/InnerProductExamples.scala
@@ -2,42 +2,57 @@
 object InnerProductExamples {
 
   import InnerProduct._
+  val list1 = 1 to 50
+  val list2 = 1 to 50
+  def main(args: Array[String]): Unit = {
 
-  val list1 = List(1,8,5)
-  val list2 = List(4, 7,9)
+    var times = collection.mutable.Map[String,Double]()
 
-  var t0 = System.nanoTime()
-  val res = innerProduct_For[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
-  var t1 = System.nanoTime()
-  println("Result using for = "+res + (t1 - t0) + "ns")
+    var t0 = System.nanoTime()
+    val res = innerProduct_For[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
+    var t1 = System.nanoTime()
+    times += ("for" -> (t1 - t0)/1000000.0)
+    println("Result using for = "+res +" during " + (t1 - t0)/1000000.0 + " s")
+    t0 = System.nanoTime()
+    val res2 = innerProduct_Next[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
+    t1 = System.nanoTime()
+    println("Result using next = "+res2 + " during " + (t1 - t0)/1000000.0 + " s")
+    times += ("next" -> (t1 - t0)/1000000.0)
+    t0 = System.nanoTime()
+    val res3 = innerProduct_While[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
+    t1 = System.nanoTime()
+    println("Result using while = "+res3 + " during " + (t1 - t0)/1000000.0 + " s")
+    times += ("while" -> (t1 - t0)/1000000.0)
+    t0 = System.nanoTime()
+    val res4 = innerProduct_Map[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
+    t1 = System.nanoTime()
+    println("Result using map = "+res4  + " during " +(t1 - t0)/1000000.0 + " s")
+    times += ("map" -> (t1 - t0)/1000000.0)
+    t0 = System.nanoTime()
+    val res5 = innerProduct_FoldLeft[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
+    t1 = System.nanoTime()
+    println("Result using fold left = "+res5  + " during " + (t1 - t0)/1000000.0 + " s")
+    times += ("fold-left" -> (t1 - t0)/1000000.0)
+    t0 = System.nanoTime()
+    val res6 = innerProduct_Foreach[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
+    t1 = System.nanoTime()
+    println("Result using foreach = "+res6  + " during " + (t1 - t0)/1000000.0 + " s")
+    times += ("forEach" -> (t1 - t0)/1000000.0)
+    t0 = System.nanoTime()
+    val res7 = innerProduct_Recursion[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
+    t1 = System.nanoTime()
+    println("Result using recursion = "+res7  + " during " + (t1 - t0)/1000000.0 + " s")
+    times += ("recursion" -> (t1 - t0)/1000000.0)
+    println("*************************")
+    // Gets the way which calculate the result of inner product in best performance
+    val min = times.values.min
+    var way : String = ""
+    times.keys.foreach{ x => if (min == times.get(x).get) way = x  }
+    println("The minimum time is "  + min + " with " + way )
+    // Gets the way which calculate the result of inner product in bad performance
+    val max = times.values.max
+    times.keys.foreach{ x => if (max == times.get(x).get) way = x  }
+    println("The maximum time is "  + max + " with " + way )
+  }
 
-  t0 = System.nanoTime()
-  val res2 = innerProduct_Next[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
-  t1 = System.nanoTime()
-  println("Result using next = "+res2 + (t1 - t0) + "ns")
-
-  t0 = System.nanoTime()
-  val res3 = innerProduct_While[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
-  t1 = System.nanoTime()
-  println("Result using while = "+res3 + (t1 - t0) + "ns")
-
-  t0 = System.nanoTime()
-  val res4 = innerProduct_Map[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
-  t1 = System.nanoTime()
-  println("Result using map = "+res4 + (t1 - t0) + "ns")
-
-  t0 = System.nanoTime()
-  val res5 = innerProduct_FoldLeft[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
-  t1 = System.nanoTime()
-  println("Result using fold left = "+res5 + (t1 - t0) + "ns")
-
-  t0 = System.nanoTime()
-  val res6 = innerProduct_Foreach[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
-  t1 = System.nanoTime()
-  println("Result using foreach = "+res6 + (t1 - t0) + "ns")
-
-  t0 = System.nanoTime()
-  val res7 = innerProduct_Recursion[Int, Int, Int, Int](list1)(list2)(_ * _)(_ + _)(0)
-  t1 = System.nanoTime()
-  println("Result using recursion = "+res7 + (t1 - t0) + "ns")
 }
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,15 +1,24 @@
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.FunSpec
+class Test extends FunSpec {
+  describe("The Sequence 1 :from " +InnerProductExamples.list1) {
+    describe("(when empty)") { // All tests within these curly braces are about "A Set (when empty)"
+      it("should have size 0") { // Here, 'it' refers to "A Set (when empty)". The full name
+        assert(InnerProductExamples.list1.size == 0) // of this test is: "A Set (when empty) should have size 0"
+      }
+      it("should produce NoSuchElementException when head is invoked") { // Define another test
+        intercept[NoSuchElementException] {
+          InnerProductExamples.list1.head
+        }
+      }
 
-class Test extends FlatSpec with Matchers {
-
-  "Lenght of list1" should "be equals lenght of list2" in {
-    assert(InnerProductExamples.list1.length == InnerProductExamples.list2.length)
-  }
-
-  it should "consistent in size " in {
-    a[java.lang.AssertionError] should be thrownBy {
-      assert(InnerProductExamples.list1.length != InnerProductExamples.list2.length)
     }
-
+  }
+  describe("(when in consistent )") {
+    it("should be non-empty") {
+      assert(InnerProductExamples.list1.size != 0 && InnerProductExamples.list2.size != 0)
+    }
+    it("should have the same size") {
+      assert(InnerProductExamples.list1.size == InnerProductExamples.list2.size)
+    }
   }
 }
